5b96631142da385fd84396b1d433930cad1eac8e
Marco Sanchez <Notmarcosan@gmail.com>
2023-05-08 15:48:43 -0400

    Array lesson complete

---
+++ lecturedemos/2-11.js
@@ -0,0 +1,19 @@
+"use strict";
+
+
+let kids = [
+    "Marco",
+    "Tom",
+    "Irvin"
+];
+// console.log(kids)
+// console.log(kids[2])
+// console.log(kids[1])
+// console.log(kids[0])
+
+let i = 0;
+let countOfKids = kids.length;
+
+for(i = 0; i < countOfKids; i++){
+    console.log(kids[i]);
+}
